8a0115c6f5274285ea8f3a89c6b6480e3c0182f0
Guilherme Campos <guilhermeozana@hotmail.com>
2025-04-23 11:18:22 -0300

    chore: configuring unit tests

--- src/DomeGym.Domain/Participant.cs
+++ src/DomeGym.Domain/Participant.cs
@@ -2,8 +2,14 @@ namespace DomeGym.Domain;
 
 public class Participant
 {
-    private readonly Guid _id;
     private readonly Guid _userId;
-    private readonly List<Guid> _sessionIds;
-     
+    private readonly List<Guid> _sessionIds = new();
+
+    public Guid Id { get; }
+    
+    public Participant(Guid userId, Guid? id = null)
+    {
+        Id = id ?? Guid.NewGuid();
+        _userId = userId;
+    }
 }
