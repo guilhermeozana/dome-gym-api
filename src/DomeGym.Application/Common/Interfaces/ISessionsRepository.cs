f9d0ecb6dd44ac2b02193c1ee414ecc492ba6621
Guilherme Campos <guilhermeozana@hotmail.com>
2025-04-29 09:29:42 -0300

    feat: add repository pattern

---
+++ src/DomeGym.Application/Common/Interfaces/ISessionsRepository.cs
@@ -0,0 +1,10 @@
+using DomeGym.Domain.SessionAggregate;
+
+namespace DomeGym.Application.Common.Interfaces;
+
+public interface ISessionsRepository
+{
+    Task AddSessionAsync(Session session);
+    
+    Task UpdateSessionAsync(Session session);
+}
